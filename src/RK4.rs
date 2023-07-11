3246be73ca1c42e71c54156df614aa8d3d4f2152
Yahli Mariansky <yahlimem@gmail.com>
2023-07-11 17:16:33 +0300

    rum

---
+++ src/RK4.rs
@@ -0,0 +1,16 @@
+use nannou::prelude::*;
+
+pub struct RK4{
+    pub position: Vec2,
+}
+
+impl RK4 {
+
+    pub fn solver(x: f32, current_time: f32, dt: f32, f: fn(f32, f32) -> f32) -> f32{
+        let k1 = f(current_time ,x);
+        let k2 = f(current_time + dt/2.0, x + (dt/2.0)*k1);
+        let k3 = f(current_time + dt/2.0, x + (dt/2.0)*k2);
+        let k4 = f(current_time + dt, x + (dt*k3));
+        dt*(k1 + 2.0*k2 + 2.0*k3 + k4)/6.0
+    }
+}
---
+++ src/springs.rs
@@ -0,0 +1,51 @@
+use nannou::prelude::*;
+use crate::RK4::RK4;
+
+#[derive(Debug)]
+pub struct Spring {
+    pub position:f32,
+    pub velocity:f32, // position prime
+    // pub force: f32,
+    // pub mass: f32,
+    // pub rest_length: f32,
+    // pub stiffness: f32,
+    // pub friction: f32,
+}
+// https://www.myphysicslab.com/explain/what-is-a-diff-eq-en.html
+
+impl Spring {
+    pub fn update(&mut self, time: f32, dt: f32){
+        // in a 2d graph y = position, x = time
+        self.velocity += RK4::solver(self.position, time, dt, Self::calculate_spring_force);
+        self.velocity += RK4::solver(self.velocity, time, dt, Self::calculate_dampen_force);
+        self.position += RK4::solver(self.velocity, time, dt, Self::calculate_position);
+    }
+    // according to the position it will calculate the force applied to the spring mass
+    pub fn calculate_spring_force(x: f32, time: f32) -> f32{
+        // if the origin of the spring is in the origin we can just use x itself
+        let force = -1.0 * x;
+
+        force
+    }
+    // according to the velociy it will calculate the dampening force applied to the spring mass
+    pub fn calculate_dampen_force(x: f32, time: f32) -> f32{
+        // if the origin of the spring is in the origin we can just use x itself
+        let force = -0.5 * x;
+
+        force
+    }
+
+    pub fn calculate_position(x: f32, time: f32) -> f32 {
+        x
+    }
+
+    pub fn f1(x: f32, time: f32){
+        
+    }
+}
+
+impl Spring {
+    pub fn create(position: f32, velocity: f32) -> Self{
+        Spring { position, velocity }
+    }
+}
